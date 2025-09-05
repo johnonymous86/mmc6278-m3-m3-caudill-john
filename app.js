1d718b0af222732ece939132eaa6ed82bd547370
John Caudill <jcaudill86@gmail.com>
2025-09-05 11:52:57 -0400

    initial push

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-// TODO: import the getCityInfo and getJobs functions from util.js
+const express = require('express');
+const path = require('path');
+const { getCityInfo, getJobs } = require('./util.js');
 
-// TODO: Statically serve the public folder
+const app = express();
 
-// TODO: declare the GET route /api/city/:city
-// This endpoint should call getCityInfo and getJobs and return
-// the result as JSON.
-// The returned JSON object should have two keys:
-// cityInfo (with value of the getCityInfo function)
-// jobs (with value of the getJobs function)
-// If no city info or jobs are found,
-// the endpoint should return a 404 status
+// Statically serve the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = app
+// Declare the GET route /api/city/:city
+app.get('/api/city/:city', async (req, res) => {
+  const location = req.params.city;
+
+  try {
+    const cityInfo = await getCityInfo(location);
+    const jobs = await getJobs(location);
+
+    // If both cityInfo and jobs are falsy, return a 404.
+    if (!cityInfo && !jobs) {
+      return res.status(404).json({
+        error: "City info and jobs not found for the given location."
+      });
+    }
+
+    // Otherwise, return a 200 status with the available data.
+    res.json({ cityInfo, jobs });
+
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({
+      error: "An internal server error occurred."
+    });
+  }
+});
+
+module.exports = app;
